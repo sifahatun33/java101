e204ad680cd495a03a959cde9546ae8a192f4714
sifahatun33 <cingozayse410@gmail.com>
2024-05-18 18:05:15 +0300

    Add files via upload

---
+++ ort.java
@@ -0,0 +1,19 @@
+package dizinin_ortalaması;
+
+import java.util.Arrays;
+
+public class ort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] dizi= {5,12,3,7,8,9};
+		int sum=0;
+	for(int i=0;i<dizi.length;i++) {
+		sum+=dizi[i];
+	}
+		System.out.println("toplam: "+sum);
+	
+        
+	}
+
+}
